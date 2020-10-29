1569424244f4eb1bbb353f496540ea51f72268e4
Eyre_S <Sukazyo@outlook.com>
2020-10-29 18:23:06 +0800

    java 5 - 添加基础的 Http 请求功能
    - 添加 HttpRequest 及 sendGetRequest(url, userAgent) 方法
    - 添加 HttpResponse 作为标准的请求回应类
      - 带有响应代码，响应信息，时间，内容及其类型和编码，发送的Cookie，完整响应头
    - 添加 WebCommonUserAgent 枚举类
      - 包含常见的 CHROME, FIREFOX, SAFARI, IE10, IE6, KITKAT(?), OPERA
      - 通过 NULL 实现自定义 userAgent
    - 添加 HttpCookies 类
      - 以复数形式储存 cookie 记录
      - 实现了从HTTP响应头解析
      - 暂时只完成了 data, host(domain), path

---
+++ java/src/main/java/cc/sukazyo/util/web/HttpCookies.java
@@ -0,0 +1,158 @@
+package cc.sukazyo.util.web;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Cookie 数据
+ *
+ * 可以储存复数条 Cookie 记录
+ *
+ * TODO hostOnly, secure, httpOnly 等属性待支持
+ */
+public class HttpCookies {
+	
+	public static class CookieValue {
+		String data;
+		String host;
+		String path;
+		long expireDate;
+		boolean hostOnly = false;
+//		boolean session = false;
+		boolean secure = false;
+		boolean httpOnly = false;
+		
+		public CookieValue (String data, String host, String path, long expireDate) {
+			this.data = data;
+			this.host = host;
+			this.path = path;
+			this.expireDate = expireDate;
+		}
+		
+		@Override
+		public String toString () {
+			return String.format("{%s, %s%s, %d, %b %b %b}", data, host, path, expireDate, hostOnly, secure, httpOnly);
+		}
+		
+	}
+	
+	private final Map<String, CookieValue> data;
+	
+	public static final Matcher requestMatcher = Pattern.compile("Set-Cookie=\\[(.*?)]").matcher("");
+	public static final Matcher dateMatcher = Pattern.compile("expires=(.*?);").matcher("");
+	
+	/* 下列是用于解析 HTTP 编码的Cookie组时使用的正则匹配器 */
+	public static final Matcher expiresMatcher = Pattern.compile("expiresLong=(.*?);").matcher("");
+	public static final Matcher maxAgeMatcher = Pattern.compile("Max-Age=(.*?);").matcher("");
+	public static final Matcher domainMatcher = Pattern.compile("domain=(.*?);").matcher("");
+	public static final Matcher pathMatcher = Pattern.compile("path=(.*?);").matcher("");
+	
+	public HttpCookies () {
+		data = new HashMap<>();
+	}
+	
+	/**
+	 * 添加一条 Cookie 记录
+	 * 已存在的 Cookie 记录将会被覆写
+	 *
+	 * @param key Cookie键
+	 * @param value Cookie值和附加数据
+	 * @return 更新后的自身
+	 */
+	public HttpCookies append (String key, CookieValue value) {
+		data.put(key, value);
+		return this;
+	}
+	
+	/**
+	 * 合并 Cookie 组
+	 * 已存在的 Cookie 条目会被覆写
+	 *
+	 * @return 更新后的自身
+	 */
+	public HttpCookies append (HttpCookies cookies) {
+		data.putAll(cookies.data);
+		return this;
+	}
+	
+	@Override
+	public String toString () {
+		StringBuilder str = new StringBuilder("{{{");
+		data.forEach( (k, v) -> str.append(k).append(v).append(",") );
+		return str.append("}}}").toString();
+	}
+	
+	public String toStringMultiLine() {
+		StringBuilder str = new StringBuilder();
+		data.forEach( (k, v) -> str.append(k).append(v).append("\n") );
+		return str.deleteCharAt(str.length()-1).toString();
+	}
+	
+	/**
+	 * 从HTTP请求头中提取出发送的Cookie数据
+	 *
+	 * @param header HTTP请求头
+	 *
+	 * @return HttpCookies 从HTTP请求头中提取出的需要更新的Cookie组对象
+	 */
+	public static HttpCookies matchCookiesFromHeader (Map<String, List<String>> header) {
+		String strData = header.toString();
+		requestMatcher.reset(strData);
+		if (requestMatcher.find()) {
+			String strCookies = requestMatcher.group(1);
+			return parseCookiesStr(strCookies);
+		}
+		return null;
+	}
+	
+	/**
+	 * 将以 HTTP 方案编码的Cookie组解析为Cookie组对象
+	 *
+	 * TODO 由于时区问题，过期时间可能出现不正确的现象
+	 *
+	 * @param strCookies 以 HTTP 方案编码的字符串Cookie组
+	 *
+	 * @return HttpCookies 解析出来的Cookie组
+	 */
+	public static HttpCookies parseCookiesStr (String strCookies) {
+		do {
+			dateMatcher.reset(strCookies);
+			if (dateMatcher.find()) {
+				SimpleDateFormat sdf = new SimpleDateFormat("EEE, dd-MMM-yyyy HH:mm:ss 'GMT'", Locale.US);
+				try {
+					strCookies = dateMatcher.replaceFirst("expiresLong=" + sdf.parse(dateMatcher.group(1)).getTime() + ";");
+				} catch (ParseException e) {
+					e.printStackTrace();
+				}
+			} else break;
+		} while (true);
+		HttpCookies cookies = new HttpCookies();
+		strCookies = strCookies.replaceAll(" ", "");
+		for (String single : strCookies.split(",")) {
+			String meta = single.substring(single.indexOf(';')+1) + ';';
+			long expire;
+			if (maxAgeMatcher.reset(meta).find()) {
+				expire = Long.parseLong(maxAgeMatcher.group(1)) + System.currentTimeMillis();
+			} else {
+				expire = Long.parseLong(expiresMatcher.reset(meta).find()?expiresMatcher.group(1):"-1");
+			}
+			cookies.append(
+					single.substring(0, single.indexOf('=')),
+					new CookieValue(
+							single.substring(single.indexOf('=')+1, single.indexOf(';')),
+							domainMatcher.reset(meta).find()?domainMatcher.group(1):"",
+							pathMatcher.reset(meta).find()?pathMatcher.group(1):"",
+							expire
+					)
+			);
+		}
+		return cookies;
+	}
+	
+}
---
+++ java/src/main/java/cc/sukazyo/util/web/WebCommonUserAgent.java
@@ -0,0 +1,34 @@
+package cc.sukazyo.util.web;
+
+public enum WebCommonUserAgent {
+	CHROME("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36"),
+	FIREFOX("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0"),
+	SAFARI("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A"),
+	IE10("Mozilla/5.0 (MSIE 10.0; Windows NT 6.1; Trident/5.0)"),
+	IE6("Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0; WOW64; Trident/4.0; SLCC1)"),
+	KITKAT("Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.114 Mobile Safari/537.36"),
+	OPERA("Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14"),
+	NULL("")
+	;
+	
+	private String agentCode;
+	
+	WebCommonUserAgent(String agentCode) {
+		this.agentCode = agentCode;
+	}
+	
+	private WebCommonUserAgent setAgentCode(String agentCode) {
+		this.agentCode = agentCode;
+		return this;
+	}
+	
+	public static WebCommonUserAgent customUserAgent(String agentCode) {
+		return NULL.setAgentCode(agentCode);
+	}
+	
+	@Override
+	public String toString() {
+		return agentCode;
+	}
+	
+}
