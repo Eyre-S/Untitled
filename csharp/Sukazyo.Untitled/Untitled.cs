823013335cf46f17edb16ece6a09badf9addbbf1
Eyre_S <Sukazyo@outlook.com>
2021-02-04 18:24:04 +0800

    csharp 1 - 建立 csharp 解决方案和 Sukazyo.Untitled 项目
    - 使用 JetBrains Rider 建立 csharp 解决方案和 Sukazyo.Untitled 项目
    - 给解决方案添加 .gitignore
    - 建立 Untitled 主类和 ProjectName, BuildVersion

---
+++ csharp/Sukazyo.Untitled/Untitled.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Sukazyo.Untitled {
+
+	public class Untitled {
+		
+		/** 项目名 */ 
+		public const String ProjectName = "CSharp Untitled";
+		/** 构建版本号 */
+		public const int BuildVersion = 1;
+		
+	}
+
+}
